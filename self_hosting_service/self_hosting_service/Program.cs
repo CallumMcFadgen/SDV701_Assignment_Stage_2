06faed2add85b04b7b0ec2c2c81b9db3d252b6ab
CallumMcFadgen <callum.mcfadgen@gmail.com>
2020-05-12 22:03:41 +1200

    database created > dummy data added > self hosting service created > tested and working :)

---
+++ self_hosting_service/self_hosting_service/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.SelfHost;
+
+namespace self_hosting_service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // CONFIGURE SERVER
+            Uri _baseAddress = new Uri("http://localhost:60064/");
+            HttpSelfHostConfiguration config = new HttpSelfHostConfiguration(_baseAddress);
+            config.Routes.MapHttpRoute(
+            name: "DefaultApi",
+            routeTemplate: "api/{controller}/{action}/{id}",
+            defaults: new { id = RouteParameter.Optional }
+            );
+
+            // CREATE SERVER
+            HttpSelfHostServer server = new HttpSelfHostServer(config);
+
+            // START SERVER
+            server.OpenAsync().Wait();
+            Console.WriteLine("TWCDatabase Web-API Self hosted on " + _baseAddress);
+            Console.WriteLine("Hit ENTER to exit...");
+            Console.ReadLine();
+            server.CloseAsync().Wait();
+        }
+    }
+}
---
+++ self_hosting_service/self_hosting_service/StoreController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace self_hosting_service
+{
+    public class StoreController : System.Web.Http.ApiController
+    {
+
+        public List<string> GetAuthorNames()
+        {
+            DataTable lcResult = clsDbConnection.GetDataTable("SELECT author_name FROM author", null);
+            List<string> lcNames = new List<string>();
+            foreach (DataRow dr in lcResult.Rows)
+                lcNames.Add((string)dr[0]);
+            return lcNames;
+        }
+
+    }
+}
